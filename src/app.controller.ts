861abde8e662610c74374f44362f194f588c6028
Vinit Kumar <vinkumar4@WKMZT005C7EB.global.publicisgroupe.net>
2024-03-07 19:13:55 +0530

    initial-commit

---
+++ src/app.controller.ts
@@ -0,0 +1,25 @@
+// app.controller.ts
+import {
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+  @Get('info')
+  getView() { 
+    return 'This is a test microservice to upload files';
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return this.appService.handleFileUpload(file);
+  }
+}
